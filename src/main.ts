d059d4942d30ba1acf49d802ba8d6c73765ee8a7
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-04-01 12:34:52 +0300

    Use prettier

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-      AppModule,
-      {
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'auth-consumer',
-          },
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'auth-consumer',
         },
       },
+    },
   );
 
   await app.listen();
