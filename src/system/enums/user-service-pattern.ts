a3e9e1e02997e508d5cc7a78d9a0f4f40eb40bb5
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-04-01 12:30:08 +0300

    Add user-service types

---
+++ src/system/enums/user-service-pattern.ts
@@ -0,0 +1,8 @@
+export const enum UserServicePattern {
+  GET_USERS = 'user-service.get.all.users',
+  GET_MY_USER = 'user-service.get.my.user',
+  GET_USER = 'user-service.get.user.by.id',
+  DELETE_USER = 'user-service.delete.user.by.id',
+  UPDATE_USER = 'user-service.update.user.by.id',
+  CREATE_USER = 'user-service.create.user',
+}
