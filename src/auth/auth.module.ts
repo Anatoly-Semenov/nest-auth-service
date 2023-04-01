c71b0d8b5546dd89d46e21b497a95dadd8629a57
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-04-01 12:48:39 +0300

    Delete unused code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
-    UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY || 'secret',
       signOptions: {
